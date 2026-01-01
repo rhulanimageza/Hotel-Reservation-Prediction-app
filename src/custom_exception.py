58062232ac1358590817f98acda780771b3879d5
MAJIN-RUU <137285166+MAJIN-RUU@users.noreply.github.com>
2026-01-01 19:57:26 +0200

    COMMITED

---
+++ src/custom_exception.py
@@ -0,0 +1,20 @@
+import traceback
+import sys
+
+class CustomException(Exception):
+
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = self.gett_detail_error_message(error_message, error_detail)
+
+    @staticmethod
+    def gett_detail_error_message(error_message, error_detail:sys):
+        
+        _ , _, exc_tb = error_detail.exc_info()
+        filename = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+        return f"Error in {filename} , Line {line_number} : {error_message}"
+    
+    def __str__(self):
+        return self.error_message
